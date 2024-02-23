41347271324483be260e4e683343dcaa56e008fe
idkyet312 <160145816+idkyet312@users.noreply.github.com>
2024-02-24 04:54:21 +1100

    Add files via upload

--- index.js
+++ index.js
@@ -1,32 +1,14 @@
-require('dotenv').config();
-const express = require('express');
-const { Client } = require('cassandra-driver');
-const app = express();
+const http = require('http');
+
+const hostname = '127.0.0.1';
 const port = 3000;
 
-
-// Mock async function to simulate fetching data from a database
-const fetchDataFromDatabase = async () => {
-  return new Promise(resolve => setTimeout(() => resolve([
-    { id: 1, name: 'John Doe', email: 'john@example.com' },
-  ]), 1000));
-};
-
-// Handler for the /data route
-app.get('/data', async (req, res) => {
-  try {
-    const data = await fetchDataFromDatabase();
-    res.status(200).json(data);
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    res.status(500).send('An error occurred while fetching data.');
-  }
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hello World 2</h1>');
 });
 
-// Add a route handler for the root path
-app.get('/', (req, res) => {
-  res.send('Welcome to the API. Access /data to fetch data.');
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
-
-
-app.listen(port, () => console.log(`Server running on http://localhost:${port}/data`));
